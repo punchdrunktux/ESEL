5c3850eff7cb6e4e4efb989da409908e96709beb
rob <rob@robs-MacBook-Pro.local>
2019-12-13 09:05:19 -0500

    Various updates

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ESEL
+
+Machines:
+1. kali
+Purpose:  Kali Rolling distro
+Hostname: kali
+IP: 10.1.1.15
+DNS: 10.1.1.1
+GW: 10.1.1.1
+
+2. ControlTower
+Purpose:  Covenant and Caldera
+Hostname: ControlTower
+IP: 10.1.1.10
+DNS: 10.1.1.1
+GW: 10.1.1.1
+
+
+TODO:
+- install Caldera on ControlTower
--- scripts/bootstrap_ct.sh
+++ scripts/bootstrap_ct.sh
@@ -15,31 +15,6 @@ install_basics() {
     apt-get install -y unzip whois jq build-essential unzip
 }
 
-fix_eth1_static_ip() {
-  # There's a fun issue where dhclient keeps messing with eth1 despite the fact
-  # that eth1 has a static IP set. We workaround this by setting a static DHCP lease.
-  echo -e 'interface "eth1" {
-    send host-name = gethostname();
-    send dhcp-requested-address 10.10.38.105;
-  }' >> /etc/dhcp/dhclient.conf
-  service networking restart
-  # Fix eth1 if the IP isn't set correctly
-  ETH1_IP=$(ifconfig eth1 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
-  if [ "$ETH1_IP" != "10.10.38.105" ]; then
-    echo "Incorrect IP Address settings detected. Attempting to fix."
-    ifdown eth1
-    ip addr flush dev eth1
-    ifup eth1
-    ETH1_IP=$(ifconfig eth1 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
-    if [ "$ETH1_IP" == "10.10.38.105" ]; then
-      echo "[$(date +%H:%M:%S)]: The static IP has been fixed and set to 192.168.38.105"
-    else
-      echo "[$(date +%H:%M:%S)]: Failed to fix the broken static IP for eth1. Exiting because this will cause problems with other VMs."
-      exit 1
-    fi
-  fi
-}
-
 install_covenant(){
   echo "[$(date +%H:%M:%S)]: Installing Covenant C2..."
 
